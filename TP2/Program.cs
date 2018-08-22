ec018a9d04acb07a7d5f4773ccaa6dd4467c8465
Juan Vera <juanfrvera@gmail.com>
2018-08-22 17:42:27 -0300

    Add project files.

---
+++ TP2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
