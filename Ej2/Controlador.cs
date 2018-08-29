42a3ae1c3b7aff9e334f9d02958ab842db65c5da
Juan Vera <juanfrvera@gmail.com>
2018-08-29 20:25:26 -0300

    180829

--- Ej2/Controlador.cs
+++ Ej2/Controlador.cs
@@ -7,6 +7,65 @@ namespace Ej2
 {
     class Controlador
     {
+        private static Cuentas cCuentas;
+
+        // M
+
+        private static void Inicializar(Cliente.TipoDocumento pTipoDocumento, string pNumeroDoc, string pNombre)
+        {
+            cCuentas = new Cuentas(new Cliente(pTipoDocumento, pNumeroDoc, pNombre));
+        }
+        public static void Inicializar()
+        {
+            Inicializar(Cliente.TipoDocumento.DNI, "38456932", "Traita");
+        }
+
+        public static double ConsultarSaldoCC()
+        {
+            return cCuentas.CuentaCorriente.Saldo;
+        }
+        public static double ConsultarSaldoCA()
+        {
+            return cCuentas.CajaAhorro.Saldo;
+        }
+
+        public static void AcreditarSaldoCC(double pSaldo)
+        {
+            cCuentas.CuentaCorriente.AcreditarSaldo(pSaldo);
+        }
+        public static bool DebitarSaldoCC(double pSaldo)
+        {
+            return cCuentas.CuentaCorriente.DebitarSaldo(pSaldo);
+        }
+        public static void AcreditarSaldoCA(double pSaldo)
+        {
+            cCuentas.CajaAhorro.AcreditarSaldo(pSaldo);
+        }
+        public static bool DebitarSaldoCA(double pSaldo)
+        {
+           return cCuentas.CajaAhorro.DebitarSaldo(pSaldo);
+        }
+        
+
+        private static bool Transferencia(Cuenta pCuentaOrigen, Cuenta pCuentaDestino, double pSaldo)
+        {
+            bool resultado = pCuentaOrigen.DebitarSaldo(pSaldo);
+            if (resultado)
+            {
+                pCuentaDestino.AcreditarSaldo(pSaldo);
+            }
+            return resultado;
+        }
+
+        public static bool TransferenciaCorrienteACaja(double pSaldo)
+        {
+            return Transferencia(cCuentas.CuentaCorriente, cCuentas.CajaAhorro, pSaldo);
+        }
+
+        public static bool TransferenciaCajaACorriente(double pSaldo)
+        {
+            return Transferencia(cCuentas.CajaAhorro, cCuentas.CuentaCorriente, pSaldo);
+        }
 
     }
 }
--- Ej2/Program.cs
+++ Ej2/Program.cs
@@ -7,20 +7,53 @@ namespace Ej2
 {
     class Program
     {
-        static int Sumatoria(params int[] args)
+        static void Main(string [] args)
         {
-            int sum = 0;
-            for (int i = 0; i < args.Length; i++)
-            {
-                sum += args[i];
+            Controlador.Inicializar();
+            Console.WriteLine("Seleccione cuenta: ");
+            Console.WriteLine("1- Cuenta Corriente");
+            Console.WriteLine("2- Caja Ahorro");
+            string opcion = Console.ReadLine();
+            switch (opcion)
+                {
+                case "1":
+                    MenuCuentaCorriente();
+                    break;
+                case "2":
+                    MenuCajaAhorro();
+                    break;
+                default:
+                    Console.WriteLine("Opcion invalida");
+                    break;
+                }
+        }
+
+        static void MenuCuentaCorriente()
+        {
+            Console.Clear();
+            Console.WriteLine("1- Consultar saldo.");
+            Console.WriteLine("2- Acreditar saldo.");
+            Console.WriteLine("3- Debitar saldo.");
+            string opcion = Console.ReadLine();
+            Console.Clear();
+            switch(opcion)
+                {
+                case "1":
+                    Console.WriteLine("Saldo disponible: {0}", Controlador.ConsultarSaldoCC());
+                break;
+                case "2":
+                    Console.WriteLine("Ingrese saldo a acreditar: ");
+                    Controlador.AcreditarSaldoCC(Convert.ToDouble(Console.ReadLine()));
+                    Console.WriteLine();
+                    Console.WriteLine("Saldo Acreditado");
+                break;
+                case "3":
+                    //COMPLETAR EL DEBITAR Y LUEGO HACER EL MENU CAJA AHORRO
+
+                default:
+                    Console.WriteLine("Opcion invalida");
+                break;
             }
-            return sum;
         }
-        static void Main(string[] args)
-        {
-            Console.WriteLine(Sumatoria(4, 5, 3, 7, 8).ToString());
-            Console.ReadKey();
-        }
-        //{0:0.00} -> Le da formato al parametro cero   
     }
 }
